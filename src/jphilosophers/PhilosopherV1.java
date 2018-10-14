5131fbcc8213143275aa30a380de1ca8bbc8319b
lukkadd <lukkadd@yahoo.com.br>
2018-10-14 01:44:28 -0300

    Visualization with deadlock

--- src/jphilosophers/PhilosopherV1.java
+++ src/jphilosophers/PhilosopherV1.java
@@ -5,20 +5,28 @@
  */
 package jphilosophers;
 
+import processing.core.*;
+
 /**
  *
  * @author lukka
  */
-public class Philosopher implements Runnable {
+public class PhilosopherV1 implements Runnable {
 
     //Forks available for this Philosopher
     private Object leftFork;
     private Object rightFork;
+    public int state;
+    public int posx;
+    public int posy;
     
     //Philosopher constructor assigns the forks
-    public Philosopher(Object leftFork,Object rightFork){
+    public PhilosopherV1(Object leftFork,Object rightFork,int posx, int posy){
         this.leftFork = leftFork;
         this.rightFork = rightFork;
+        this.state = 0;
+        this.posx = posx;
+        this.posy = posy;
     }
     
     private void doAction(String action) throws InterruptedException{
@@ -27,7 +35,7 @@ public class Philosopher implements Runnable {
             Thread.currentThread().getName() + " " + action
         );
         //spend a random amount of time doing it
-        Thread.sleep((long) (Math.random()*100));
+        Thread.sleep((long) (Math.random()*1000));
     }
     
     @Override
@@ -47,18 +55,21 @@ public class Philosopher implements Runnable {
             while(true){
                 
                 //think
+                this.state = 0;
                 doAction(System.nanoTime() + ": Thinking");
                 
                 //pick_up_left_fork
                 //synchronized makes sure only this thread will have access
                 //to this specific fork while the Philosopher is using it
                 synchronized(leftFork){
+                    this.state = 1;
                     doAction(System.nanoTime() + ": Picked up left fork");
-                
+                    
                     //pick_up_right_fork
                     synchronized(rightFork){
                         doAction(System.nanoTime() + ": Picked up right fork");
                         
+                        this.state = 2;
                         //eat
                         doAction(System.nanoTime() + ": Eating");
                     }
@@ -69,6 +80,7 @@ public class Philosopher implements Runnable {
                 
                 //put_down_left_fork
                 doAction(System.nanoTime() + ": Put down left fork");
+                this.state = 0;
             }            
         } catch(InterruptedException e){
             System.out.println("Something went wrong... " + Thread.currentThread().getName() + " Interrupted");
---
+++ src/jphilosophers/VisualizationV1.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jphilosophers;
+
+import processing.core.*;
+
+/**
+ *
+ * @author lukka
+ */
+public class VisualizationV1 extends processing.core.PApplet{
+    
+    PhilosopherV1[] philosophers;
+    
+    public void settings(){
+        size(720,480);
+    }
+    
+    public void setup(){
+        
+        //initializing philosopher and fork arrays
+        philosophers = new PhilosopherV1[5];
+        Object[] forks = new Object[5];
+        
+        //initializing Fork objects
+        for(int i = 0; i < forks.length; i++){
+            forks[i] = new Object();
+        }
+        
+        //initializing Philosopher objects
+        for(int i = 0; i < philosophers.length; i++){
+            philosophers[i] = new PhilosopherV1(
+                    forks[i],
+                    forks[(i+1) % forks.length],
+                    (width / 2) + (int) (200*Math.cos( (i * 2 * Math.PI / 5) - Math.PI / 2)),
+                    (height / 2) + (int) (200*Math.sin( (i * 2 * Math.PI / 5) - Math.PI / 2))
+            );
+            
+            //creating the philosopher thread and running it
+            Thread t = new Thread(philosophers[i],"Philosopher " + (i+1));
+            t.start();
+        }
+        
+        frameRate(60);
+    }
+    
+    public void draw(){
+        background(51);
+        ellipse(width/2,height/2,280,280);
+        rect(mouseX,mouseY,10,10);
+        for(int i = 0; i < philosophers.length; i++){
+            renderPhilosopher(philosophers[i]);
+        }
+    }
+    
+    private void renderPhilosopher(PhilosopherV1 p){
+        pushStyle();
+            switch(p.state){
+                case 0:
+                fill(100,100,100);
+                break;
+                case 1:
+                fill(140,0,0);
+                break;
+                case 2:
+                fill(0,140,0);
+                break;
+            }
+            rect(p.posx - 30,p.posy-5,60,60,30,30,0,0);
+            ellipse(p.posx,p.posy - 15,40,40);
+        popStyle();
+    }
+    
+    private void renderFork(int index){
+        
+    }
+}
